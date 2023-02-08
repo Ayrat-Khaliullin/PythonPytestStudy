22251a7ae410ea10279cee4f50f95becd5030e57
Айрат Халиуллин Р <a.khaliullin@bars-open.ru>
2023-02-08 20:14:18 +0300

    Added base_page and main_page

---
+++ pages/base_page.py
@@ -0,0 +1,20 @@
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver import Remote as RemoteWebDriver
+
+
+class BasePage:
+
+    def __init__(self, browser: RemoteWebDriver, url, timeout=10):
+        self.browser = browser
+        self.url = url
+        self.browser.implicitly_wait(timeout)
+
+    def open(self):
+        self.browser.get(self.url)
+
+    def is_element_present(self, by, value):
+        try:
+            self.browser.find_element(by, value)
+        except NoSuchElementException:
+            return False
+        return True
