f202bd7cab9f4e8fecc0b208ae439dd95e2d7edf
Айрат Халиуллин Р <a.khaliullin@bars-open.ru>
2023-02-08 19:38:41 +0300

    Added base files for the project

---
+++ conftest.py
@@ -0,0 +1,33 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as ChOptions
+from selenium.webdriver.firefox.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser_name", action="store", default="chrome",
+                     help="choose browser 'chrome' or 'firefox'")
+    parser.addoption("--language", action="store", default="en",
+                     help="choose language ru or en")
+
+
+@pytest.fixture(scope="class")
+def browser(request):
+    browser_name = request.config.getoption("--browser_name")
+    user_language = request.config.getoption("--language")
+    if browser_name == "chrome":
+        print("\nstart chrome for test..")
+        options = ChOptions()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        browser = webdriver.Chrome(options=options)
+    elif browser_name == "firefox":
+        print("\nstart chrome for test..")
+        options = Options()
+        options.set_preference('intl.accept_languages', user_language)
+        browser = webdriver.Firefox(options=options)
+
+    else:
+        pytest.UsageError("--browser_name should be chrome or firefox")
+    yield browser
+    print("\nclose browser for test..")
+    browser.quit()
